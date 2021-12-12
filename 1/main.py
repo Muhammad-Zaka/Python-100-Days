9344bc9dbd1545e3a6a3d2578edbc83da578c138
muhammad-zaka <m.zkaurrehman@gmail.com>
2021-12-13 02:10:20 +0500

    Typo Fix

--- 1/main.py
+++ 1/main.py
@@ -26,7 +26,7 @@
 # print("second_variable= " + second_variable)
 
 
-print("Welcone to the Band Name Generator\n")
+print("Welcome to the Band Name Generator\n")
 name=input("What's name of the city you grew up in?\n")
 pet_name=input("What is your pet name?\n") 
 print("Your band name could be " + name + " " + pet_name )
