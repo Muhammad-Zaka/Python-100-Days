f9a015635af9d1dd1243d00dae19879f60796b55
muhammad-zaka <m.zkaurrehman@gmail.com>
2021-12-13 01:24:39 +0500

    First Part Done

--- 1/main.py
+++ 1/main.py
@@ -1,1 +1,33 @@
-print("hello world")
+
+## Print Function
+
+#print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
+#print("The function is declared like this:\n")
+#print("print('what to print')")
+#print("Hello " + "Zaka")
+
+## Input Function
+#input("What is your name?")
+#print("Hello "+ input("What is your name?"))
+#print(len(input("What is your name? ")))
+
+## Variables
+#name=input("What is your name? ")
+#name="Rehman"
+#print(name)
+
+
+# first_variable=input("first_variable: ")
+# second_variable=input("second_variable: ")
+# temp=first_variable
+# first_variable=second_variable
+# second_variable=temp
+# print("first_variable= " + first_variable)
+# print("second_variable= " + second_variable)
+
+
+print("Welcone to the Band Name Generator\n")
+name=input("What's name of the city you grew up in?\n")
+pet_name=input("What is your pet name?\n") 
+print("Your band name could be " + name + " " + pet_name )
+
